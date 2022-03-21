d4aca972f1433480b5b8e3299d03aca06ae4d90e
“Alexis <alexis.bougy@devinci.fr>
2022-03-21 17:34:46 +0100

    Portail

---
+++ cours/menu.php
@@ -0,0 +1,8 @@
+<nav class="menu">
+    <p>Portail</p>
+    <ul>
+        <li class="menu-item"><a href="index.php">Accueil</a></li>
+        <li class="menu-item"><a href="notes.php">Notes</a></li>
+        <li class="menu-item"><a href="presence.php">Présence</a></li>
+    </ul>
+</nav>
